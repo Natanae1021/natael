d03b0ee11f3804e0843a0009614e7ccaef687269
Ancis ras <168407296+Natanae1021@users.noreply.github.com>
2024-06-05 11:10:21 +0700

    Add files via upload

---
+++ contoh.py
@@ -0,0 +1,19 @@
+from paket.fungsiluas import balok
+from paket.fungsivolume import balok1
+print(f"Luas Balok : {balok(10, 15, 30)}")
+print(f"Volume Balok : {balok1(10, 1, 30)}")
+
+from paket.fungsiluas import kubus
+from paket.fungsivolume import kubus1
+print(f"Luas kubus : {kubus(4)}")
+print(f"volume kubus : {kubus1(4)}")
+
+from paket.fungsiluas import limassegiempat
+from paket.fungsivolume import limassegiempat1
+print(f"Luas limas segi empat : {limassegiempat(5, 6, 8)}")
+print(f"volume limas segi empat : {limassegiempat1(5, 8, 6)}")
+
+from paket.fungsiluas import bola
+from paket.fungsivolume import bola1
+print(f"Luas bola : {bola(3)}")
+print(f"volume bola : {bola1(3)}")
